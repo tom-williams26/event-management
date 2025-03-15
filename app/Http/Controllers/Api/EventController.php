0dca15c2f2f2c802de986c8343c14beaa770d822
Tom Williams <tom.williams0593@outlook.com>
2025-03-15 04:29:29 +0000

    Storing and validating Event data. Tested using Postman.

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
+
 class EventController extends Controller
 {
     /**
@@ -21,7 +22,18 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time',
+            ]),
+            'user_id' => 1
+        ]);
+        return $event;
     }
 
     /**
