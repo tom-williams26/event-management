962bd299fb16ba9fcc7c195dd91cab845fefdddd
Tom Williams <tom.williams0593@outlook.com>
2025-03-19 23:55:05 +0000

    Created API resources for Event, User & Attendees. Provides support with customizing responses using Postman.

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection((Event::with('user')->get()));
     }
 
     /**
@@ -33,7 +34,7 @@ class EventController extends Controller
             ]),
             'user_id' => 1
         ]);
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -41,7 +42,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
