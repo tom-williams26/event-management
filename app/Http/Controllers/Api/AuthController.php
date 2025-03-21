69b12a0bb7ae12a54ca15968b6b96ba8440e287e
Tom Williams <tom.williams0593@outlook.com>
2025-03-21 05:11:00 +0000

    Added logout logic to AuthController. Added a expiration to session tokens.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -38,5 +38,12 @@ class AuthController extends Controller
         ]);
     }
 
-    public function logout(Request $request) {}
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
