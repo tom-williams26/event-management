8fe5221e7e26a6cdd691461398ac03c3586e4f45
Tom Williams <tom.williams0593@outlook.com>
2025-03-27 03:27:58 +0000

    Recreated the docker container, attempted to queue jobs/emails, however jobs are not queuing. Finally setting a rate limit to both Event & Attendee controllers to finish this project.

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -21,6 +21,8 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
 
@@ -45,7 +47,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                'user_id' => 1
+                'user_id' => $request->user()->id
             ])
         );
 
@@ -75,8 +77,6 @@ class AttendeeController extends Controller
      */
     public function destroy(Event $event, Attendee $attendee)
     {
-        // Gate::authorize('delete-attendee', [$event, $attendee]);
-
         $attendee->delete();
 
         return response(status: 204);
